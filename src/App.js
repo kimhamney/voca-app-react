e66c853e80873ab2672815027831ae645316254b
kimhamney <kimhamney@gmail.com>
2024-01-02 07:57:20 +0900

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import Home from './components/home.js'
+import ReactGA from 'react-ga'
+
+const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID
+ReactGA.initialize(gaTrackingId, { debug: true })
+ReactGA.pageview(window.location.pathname)
 
 const App = () => {
     return <Home />
