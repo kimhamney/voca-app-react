389ccb35ded71f2db352c23c80231a58ab61fc1a
kimhamney <kimhamney@gmail.com>
2024-01-07 14:40:43 +0900

    fix: google analytics tracking id

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from './components/home.js'
 import ReactGA from 'react-ga4'
 
-const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID
+const gaTrackingId = 'G-7Z37WVGPVZ'
 ReactGA.initialize(gaTrackingId)
 ReactGA.set({ page: window.location.pathname })
 ReactGA.send('pageview')
